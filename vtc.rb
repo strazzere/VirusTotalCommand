85ebe0586fe720998137f6bfd06b75ec1d6de9ba
Tim Strazzere <Tim.Strazzere@mylookout.com>
2012-01-18 16:25:33 -0800

    Fixing some of the basic regex's used - should be functional now.

--- vtc.rb
+++ vtc.rb
@@ -3,13 +3,18 @@ require 'rubygems'
 require 'mechanize'
 require 'json'
 require 'digest/md5'
-require 'digest/sha1'
+require 'digest/sha2'
 
-VER = "1.1"
+VTC_VERSION = "1.1"
 
-LATEST = "http://www.virustotal.com/latest-report.html?resource="
-UPDATES = "http://www.virustotal.com/get_updates.json?_="
+LATEST = "https://www.virustotal.com/file/"
+LATEST_MID = "/analysis/"
 AGENT = 'Mac Safari'
+
+FILE_NAMES = /<h5>File names <small>\(max. 25\)<\/small><\/h5>[<ol>\n|\s]+<li>[\n|\s]+([^\n]+)[\n|\s]+<\/li>/
+FIRST_SEEN = /<td>[\n|\s]+<h5>First seen by VirusTotal<\/h5>[\n|\s]+([\d\-\s\:]+UTC)\s\(\s([\d]+\s[\w\s]+)/
+LAST_SEEN  = /<td>\n\s{15}<h5>Last seen by VirusTotal<\/h5>\n\s{15}([\d\-\s\:]+UTC)\s\(\s([\d]+\s[\w\s]+)/
+DETECTION_RATIO = /<td>Detection ratio:<\/td>[\n|\s]+<[\w\s=\"\-]+>(\d\d\s\/\s\d\d)<\/td>/
 
 def help
   puts "Commands --"
@@ -34,69 +39,52 @@ def verbose (string=nil)
   end
 end
 
-
-def latest_for_hash (hash=nil)
-  if(!hash.nil?)
-    mech = Mechanize.new
-    mech.get LATEST + hash do |search|
-      file = []
-      file<< {:file_name => search.content.scan(/<span id=\"status-object\" class=\"blackthick\">(.*?)<\/span>/)}
-      file<< {:submission_date => search.content.scan(/<span id=\"status-date\" class=\"blackthick\">(.*?)<\/span>/)}
-      file<< {:submission_status => search.content.scan(/<span id=\"status-status\" class=\"blackthick\">(.*?)<\/span>/)}
-      file<< {:detection_num => search.content.scan(/<span id=\"porcentaje\" style=\"color: red\">([0-9]+)<\/span>/)}
-      file<< {:total_scanned_with => search.content.scan(/<span id=\"status-total\">\/([0-9]+) \(([0-9.]+)%\)<\/span>/)[0][0]}
-      file<< {:detection_percentage => search.content.scan(/<span id=\"status-total\">\/([0-9]+) \(([0-9.]+)%\)<\/span>/)[0][1]}
-      file<< {:community_percentage => search.content.scan(/<span style=\"font-size: 0.8em;\">&nbsp;Safety score: ([0-9.]+)%&nbsp;<\/span>/)}
-      puts file.to_json
-    end
+def get_information (hash=nil, time=nil)
+  if(hash.nil?)
+    raise RuntimeError.new 'The hash (sha256) of the sample is required to get information about it!'
   end
-end
 
-def get_updates (time=nil)
-  mech = Mechanize.new
-  mech.user_agent_alias = AGENT
   if(time.nil?)
     time = Time.now.to_i.to_s
+    verbose 'No time provided, using current time of [ ' + time.to_s + ' ]'
   end
-  mech.get UPDATES + time do |updates|
-    return JSON.parse(updates.content.to_s)
-  end
-end
 
-def display_updates
-  info "Querying for updates..."
-  status = get_updates nil
-  if(!status.nil?)
-    if(!status['updates'].nil?)
-      if(status['updates'] == 0)
-        update_str = "No updates found."
-      else
-        update_str = "Updates found!"
-      end
-      verbose update_str + " Updates variable: [ " + status['updates'].to_s + " ]"
-    end
-    if(!status['detail'].nil?)
-      if(status['detail'] == "")
-        detail_str = "No details found. "
-      else
-        detail_str = "Details found, (not normal)!"
-      end
-      verbose detail_str + " Details: [ " + status['detail'].to_s + " ]"
-    end
+  mech = Mechanize.new
+  file = []
+  mech.get(LATEST + hash + LATEST_MID + Time.now.to_i.to_s + '/') do |search|
+    file<< {
+      :file_names => search.content.scan(FILE_NAMES)[0][0]
+    }
+    file<< {
+      :first_seen_utc => search.content.scan(FIRST_SEEN)[0][0],
+      :first_seen_human => search.content.scan(FIRST_SEEN)[0][1]
+    }
+    file<< {
+      :last_seen_utc => search.content.scan(LAST_SEEN)[0][1],
+      :last_seen_human => search.content.scan(LAST_SEEN)[0][1]
+    }
+    file<< {
+      :detection_ratio => search.content.scan(DETECTION_RATIO)[0][0]
+    }
   end
+
+  return file
 end
 
 if $stdin.tty?
-  info "VTCommand v" + VERSION + " - Tim Strazzere (strazz@gmail.com)"
+  info "VTCommand v" + VTC_VERSION + " - Tim Strazzere (strazz@gmail.com)"
   if(ARGV.length == 0)
     help
   end
   ARGV.each do |file|
     begin
-      latest_for_hash "5192ad05597e7a148f642be43f6441f6"
+      verbose 'Processing file [ ' + file.to_s + ' ]'
       md5_digest = Digest::MD5.hexdigest(File.read(file))
-      sha_digest = Digest::SHA1.hexdigest(File.read(file))
-      display_updates
+      sha_digest = Digest::SHA2.hexdigest(File.read(file))
+      verbose "MD5:\t[ " + md5_digest.to_s + " ]"
+      verbose "SHA256:\t[ " + sha_digest.to_s + " ]"
+      info = get_information sha_digest.to_s
+      puts info
     rescue Errno::ENOENT
       error "\'" + file + "\' was not found!"
     end
